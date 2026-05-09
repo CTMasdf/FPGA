d4c2880b922eff3e7a65bfab2215cb8d31ab8ec9
CTMasdf <90117641+CTMasdf@users.noreply.github.com>
2026-05-09 17:11:54 +0900

    2학년 1학기 11주차

---
+++ First_week11_240516/week11_ex04_CTM.vhd
@@ -0,0 +1,49 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- week11
+-- week11_ex04_CTM
+-- 2학년_2반_2021041089_최태민
+
+entity week11_ex04_CTM is
+    Port ( X : in  STD_LOGIC_VECTOR (3 downto 0);	-- X0 ~ X3 스위치
+           Y0 : out  STD_LOGIC);	-- Y0 LED
+end week11_ex04_CTM;
+
+architecture Behavioral of week11_ex04_CTM is
+	signal A: STD_LOGIC;
+	signal B: STD_LOGIC;
+	signal Y: STD_LOGIC;
+	signal Buffer_A1_B2: STD_LOGIC_VECTOR (2 downto 1);
+begin
+	A <= X(3);	-- 3state buffer 1
+	B <= X(0);	-- 3state buffer 2
+	
+	process(X, A, B)
+	begin
+		-- Buffer_A1_B2(2) = buffer 1
+		-- Buffer_A1_B2(1) = buffer 2
+		
+		if (X(2) = '0' and X(1) = '0') then 
+			Buffer_A1_B2 <= "ZZ";	
+			
+		elsif (X(2) = '0' and X(1) = '1') then 
+			Buffer_A1_B2 <= 'Z' & B;	
+			
+		elsif (X(2) = '1' and X(1) = '0') then 
+			Buffer_A1_B2 <= A & 'Z';	
+			
+		else
+			Buffer_A1_B2 <= A & 'Z';	
+			
+		end if;
+	end process;
+	
+Y <= Buffer_A1_B2(2);		
+Y <= Buffer_A1_B2(1);
+
+Y0 <= not Y;	
+
+
+end Behavioral;
+
---
+++ First_week11_240516/week11_ex04_CTM_TB.vhd
@@ -0,0 +1,66 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.std_logic_unsigned.ALL; --산술연상을 위한 패키지
+USE ieee.numeric_std.ALL;
+
+-- week11
+-- week11_ex04_CTM_TB
+-- 2학년_2반_2021041089_최태민
+ 
+ENTITY week11_ex04_CTM_TB IS
+END week11_ex04_CTM_TB;
+ 
+ARCHITECTURE behavior OF week11_ex04_CTM_TB IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT week11_ex04_CTM
+    PORT(
+         X : IN  std_logic_vector(3 downto 0);
+         Y0 : OUT  std_logic
+        );
+    END COMPONENT;
+   --Inputs
+   signal X : std_logic_vector(3 downto 0) := (others => '0');
+    
+
+
+ 	--Outputs
+   signal Y0 : std_logic;
+	signal XW : std_logic_vector(1 downto 0) := "00";
+	signal Buffer_AB : std_logic_vector(1 downto 0) := "00";
+	
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: week11_ex04_CTM PORT MAP (
+          X => X,
+          Y0 => Y0
+        );
+
+	Buffer_AB_proc: process
+	begin
+	
+		X(2) <= Buffer_AB(1);	-- 3state buffer 2 switch
+		X(1) <= Buffer_AB(0);	-- 3state buffer 1 switch
+		Buffer_AB <= Buffer_AB + 1;
+		
+		wait for 400 ns;
+		
+		if(X = "1111") then wait;
+		end if;
+	end process;
+	
+   stim_proc: process
+   begin		
+      X(0) <= XW(0);
+		X(3) <= XW(1);
+		XW <= XW + 1;	--increase vector x value
+		
+		wait for 100 ns;	--100ns delay
+		
+		if(X = "1111") then wait;
+		end if;
+   end process;
+
+END;
